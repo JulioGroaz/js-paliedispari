c2f92bc4c5153604fabd7f2a063ce66ef75ea201
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-16 16:01:41 +0200

    esercizio 2 completato

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+
+//primo esercizio: PALINDROMA:
+
 function palindroma (parola){
     let inverseParola = '';
     const string = parola + '';
@@ -15,3 +18,35 @@ function palindroma (parola){
 
 const parolaUtente= prompt('inserisci una parola');
 console.log(palindroma(parolaUtente));
+
+//secondo esercizio PARI e DISPARI:
+
+const pariDispari = prompt('Scegli tra: pari o dispari');
+const numeroUtente = Number.parseInt(prompt('scegli un numero tra 1-5'),10);
+
+function numeroRandom (){
+    return Math.floor(Math.random()*5)+1;
+}
+
+let randomNumber = numeroRandom();
+console.log('il numero scelto dal computer è:' + randomNumber);
+
+
+const somma = numeroUtente + randomNumber;
+function sommatoriaPD (somma){
+    
+     if(somma %2 === 0){
+        return 'pari';
+     } else {
+        return 'dispari';
+     }
+}
+
+let pariODispari = sommatoriaPD(somma);
+console.log ('la somma è:'+somma);
+
+if(pariODispari === pariDispari){
+    console.log ('HAI VINTO');
+} else {
+    console.log('HAI PERSO');
+}
